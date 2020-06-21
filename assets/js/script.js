96f8b1128c536a36fca3e827cfed1758a58e3823
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-06-21 12:54:11 -0700

    timer running

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var time = 100;
+var myInterval = 1000;
+var timerStart;
+var startBtn = document.getElementById("start");
+var timerEl = document.getElementById("timer");
+
+var questions = [
+    {
+        question: "What tag is used to make an unordered bulleted list?",
+        choices: ["<ul>", "<li>", "<p>", "<ol>"],
+        answer: "<ul>"
+    },
+    {
+        question: "What tag can be used to insert a break or a blank line in HTML document?",
+        choices: ["<main></main>", "<section></section>", "<br></br>", "<body></body>"],
+        answer: "<br></br>"
+    },
+    {
+        question: "What tag specifies a field where the user can enter data?",
+        choices: ["<datalist>", "<dialog>", "<enterpoint>", "<input>"],
+        answer: "<input>"
+    },
+    {
+        question: "What is the value called that defines colors such as the following: #fff000?",
+        choices: ["RGB Value", "Color Value", "Hex Value", "Decimal Value"],
+        answer: "Hex Value"
+    },
+    {
+        question: "In JacaScript what is used in conjuction with HTML to 'react' to certain elements?",
+        choices: ["events", "boolean", "condition", "RegExp"],
+        answer: "events"   
+    }
+
+];
+
+
+
+function startQuiz() {
+    timerEl.textContent = time;
+    timerStart = setInterval(createTimer,myInterval);
+}
+
+function createTimer() {
+   time--;
+   timerEl.textContent = time;
+}
+
+startBtn.addEventListener("click", startQuiz);
+
+startQuiz();
